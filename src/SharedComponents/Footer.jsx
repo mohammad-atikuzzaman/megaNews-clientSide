c8f577013c5b62369bc4e5f4e729eddfeda3baf9
mr-codex <mcodex47@gmail.com>
2024-06-07 20:24:31 +0600

    axios installed for fetching api

--- src/SharedComponents/Footer.jsx
+++ src/SharedComponents/Footer.jsx
@@ -2,7 +2,7 @@ import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="px-4 divide-y bg-gray-800 text-gray-100">
+    <footer className="px-4 divide-y my-4 bg-gray-800 text-gray-100">
       <div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
         <div className="lg:w-1/3">
           <a
